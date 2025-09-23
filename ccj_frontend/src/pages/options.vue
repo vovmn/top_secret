a046cea689e0ffcae37da62fe72e02caf86b4c37
mon1gol <profile4634@yandex.ru>
2025-09-23 12:48:07 +0300

    add get notification switch

--- ccj_frontend/src/pages/options.vue
+++ ccj_frontend/src/pages/options.vue
@@ -2,32 +2,30 @@
   <DefaultLayout>
     <v-container>
       <h1 class="font-weight-bold mb-4">Настройки</h1>
-      <v-switch
-        v-model="isDarkTheme"
-        color="success"
-        hide-details
-        label="Темная тема"
-      />
+      <v-switch v-model="isDarkTheme" color="success" hide-details label="Темная тема" />
+      <v-switch v-model="getNotifications" color="success" hide-details label="Получать уведомления" />
     </v-container>
   </DefaultLayout>
 </template>
 
 <script setup lang="ts">
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
-  import { useTheme } from 'vuetify'
+import { useTheme } from 'vuetify'
 
-  const theme = useTheme()
+const theme = useTheme()
 
-  const isDarkTheme = computed({
-    get () {
-      return theme.global.name.value === 'dark'
+const isDarkTheme = computed({
+  get() {
+    return theme.global.name.value === 'dark'
   },
-    set (value: boolean) {
-      const newTheme = value ? 'dark' : 'light'
-      theme.global.name.value = newTheme
-      localStorage.setItem('app-theme', newTheme)
-    }
+  set(value: boolean) {
+    const newTheme = value ? 'dark' : 'light'
+    theme.global.name.value = newTheme
+    localStorage.setItem('app-theme', newTheme)
+  }
 })
+
+const getNotifications = ref(false);
 </script>
 
 <style scoped lang="sass">
