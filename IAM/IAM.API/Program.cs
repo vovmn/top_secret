d6a808fba36334b6fda8809c0bb46c3cc0a67178
WhiRav <SacraCorvus@gmail.com>
2025-10-02 06:14:41 +0300

    hh

--- IAM/IAM.API/Program.cs
+++ IAM/IAM.API/Program.cs
@@ -46,6 +46,21 @@ builder.Services.AddJWTandUserThings(builder.Configuration);
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
